5c38715e7ecc89aa5296b6249ee2ce2f76c65145
santosh100 <sahu18456@gmail.com>
2020-05-01 12:05:42 +0530

    Day - 29 Binary Tree Maximum Path Sum

---
+++ Day29.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maximum = Integer.MIN_VALUE;
+    int helper(TreeNode root){
+        if(root == null){
+            return 0;
+        }        
+        
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+        maximum = Math.max(maximum, left+right+root.val);
+        return Math.max(left,right)+root.val;
+    }
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return maximum;
+    }
+}
